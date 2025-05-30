dfad65cfc64104a440cef325b24567d2eb515997
Ashutosh <Ashutosh>
2025-05-30 15:35:02 +0530

    Added Various possible exceptions thrown while processing the request

---
+++ service-autocaption/src/main/java/com/image/autocaption/exception/EmptyFileException.java
@@ -0,0 +1,8 @@
+package com.image.autocaption.exception;
+
+public class EmptyFileException extends RuntimeException{
+
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
