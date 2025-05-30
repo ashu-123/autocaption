c1c22c502e17808ed806eb842e1d156a076ed5c7
Ashutosh <Ashutosh>
2025-05-30 15:37:19 +0530

    Added error response dto structure

---
+++ service-autocaption/src/main/java/com/image/autocaption/model/ErrorResponseDto.java
@@ -0,0 +1,51 @@
+package com.image.autocaption.model;
+
+public class ErrorResponseDto {
+
+    private String error;
+    private String message;
+    private int status;
+
+    public ErrorResponseDto(String error, String message, int status) {
+        this.error = error;
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public ErrorResponseDto setError(String error) {
+        this.error = error;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ErrorResponseDto setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public ErrorResponseDto setStatus(int status) {
+        this.status = status;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ErrorResponseDto{");
+        sb.append("error='").append(error).append('\'');
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", status=").append(status);
+        sb.append('}');
+        return sb.toString();
+    }
+}
