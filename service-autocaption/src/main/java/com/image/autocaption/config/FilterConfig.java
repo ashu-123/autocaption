5f93dace6958ebe7725c1a57d489d9b5bb7f68d5
Ashutosh <Ashutosh>
2025-05-31 19:31:33 +0530

    Added configuration to register the rate limiter filter bean

---
+++ service-autocaption/src/main/java/com/image/autocaption/config/FilterConfig.java
@@ -0,0 +1,18 @@
+package com.image.autocaption.config;
+
+import com.image.autocaption.rateLimiting.RateLimitingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<RateLimitingFilter> rateLimitingFilter(){
+        FilterRegistrationBean<RateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitingFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
+}
+
