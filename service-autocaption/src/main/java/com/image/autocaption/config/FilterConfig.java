a549d59482f5e2506544b7bcfd478d9fd6baa8ce
Ashutosh <Ashutosh>
2025-06-02 10:29:56 +0530

    Pluging in the required beans to the spring application context

--- service-autocaption/src/main/java/com/image/autocaption/config/FilterConfig.java
+++ service-autocaption/src/main/java/com/image/autocaption/config/FilterConfig.java
@@ -1,9 +1,12 @@
 package com.image.autocaption.config;
 
+import com.image.autocaption.rateLimiting.CustomRateLimitingFilter;
 import com.image.autocaption.rateLimiting.RateLimitingFilter;
+import com.rateLimiter.FixedWindowRateLimiter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
 
 @Configuration
 public class FilterConfig {
@@ -15,9 +18,32 @@ public class FilterConfig {
     }
 
     @Bean
-    public FilterRegistrationBean<RateLimitingFilter> registerRateLimitingFilter(){
-        FilterRegistrationBean<RateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(rateLimitingFilter);
+    public Jedis jedis() {
+        return new Jedis("localhost", 6379);
+    }
+
+    @Bean
+    public FixedWindowRateLimiter fixedWindowRateLimiter() {
+        return new FixedWindowRateLimiter(jedis(), 60, 10);
+    }
+
+    @Bean
+    public CustomRateLimitingFilter customRateLimitingFilter() {
+        return new CustomRateLimitingFilter(fixedWindowRateLimiter());
+    }
+
+//    @Bean
+//    public FilterRegistrationBean<RateLimitingFilter> registerRateLimitingFilter(){
+//        FilterRegistrationBean<RateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(rateLimitingFilter);
+//        registrationBean.addUrlPatterns("/api/*");
+//        return registrationBean;
+//    }
+
+    @Bean
+    public FilterRegistrationBean<CustomRateLimitingFilter> registerRateLimitingFilter() {
+        FilterRegistrationBean<CustomRateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(customRateLimitingFilter());
         registrationBean.addUrlPatterns("/api/*");
         return registrationBean;
     }
