cf0d34ac79726c1a922e3b6573f1409a31f2eee5
Ashutosh <Ashutosh>
2025-06-07 20:11:31 +0530

    Exposed an end point for JWT generation for authenticated users

---
+++ service-autocaption/src/main/java/com/image/autocaption/resource/auth/AuthResource.java
@@ -0,0 +1,51 @@
+package com.image.autocaption.resource.auth;
+
+import com.image.autocaption.model.AuthRequestDto;
+import com.image.autocaption.security.JwtUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtUtil jwtUtil;
+
+    private final UserDetailsService userDetailsService;
+
+    public AuthResource(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody AuthRequestDto request) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        final String token = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
+    }
+}
+
