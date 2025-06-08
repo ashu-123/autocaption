86402aafb08518e0fc9e2f909a3f864d232e9ac9
Ashutosh <Ashutosh>
2025-06-08 15:39:48 +0530

    Added S3 bean definition in configuration class

---
+++ service-autocaption/src/main/java/com/image/autocaption/config/s3/AwsS3Config.java
@@ -0,0 +1,19 @@
+package com.image.autocaption.config.s3;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Config {
+
+    @Bean
+    public AmazonS3 amazonS3() {
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1) // Replace with the region you selected
+                .build();
+    }
+}
