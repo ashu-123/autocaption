44a91e9916fa1bfa22cfe82508a144dbf7e0c51d
Ashutosh <Ashutosh>
2025-06-17 19:27:12 +0530

    Added /refresh end point to refresh access token

--- service-autocaption/src/main/java/com/image/autocaption/resource/auth/AuthResource.java
+++ service-autocaption/src/main/java/com/image/autocaption/resource/auth/AuthResource.java
@@ -1,7 +1,12 @@
 package com.image.autocaption.resource.auth;
 
 import com.image.autocaption.model.AuthRequestDto;
+import com.image.autocaption.model.AuthResponseDto;
+import com.image.autocaption.model.TokenRefreshRequestDto;
 import com.image.autocaption.security.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import static com.image.autocaption.security.JwtUtil.SECRET_KEY;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,19 +38,41 @@ public class AuthResource {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticate(@RequestBody AuthRequestDto request) {
+    public ResponseEntity<AuthResponseDto> authenticate(@RequestBody AuthRequestDto request) {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
-            );
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
         } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
-        final String token = jwtUtil.generateToken(userDetails);
+        final String accessToken = jwtUtil.generateAccessToken(userDetails);
+        final String refreshToken = jwtUtil.generateRefreshToken(userDetails);
 
-        return ResponseEntity.ok(Collections.singletonMap("token", token));
+        return ResponseEntity.ok(new AuthResponseDto(accessToken, refreshToken));
     }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponseDto> refresh(@RequestBody TokenRefreshRequestDto request) {
+        String refreshToken = request.getRefreshToken();
+
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(refreshToken)
+                    .getBody();
+
+            String username = claims.getSubject();
+
+            UserDetails user = userDetailsService.loadUserByUsername(username);
+            String newAccessToken = jwtUtil.generateAccessToken(user);
+            String newRefreshToken = jwtUtil.generateRefreshToken(user);
+            return ResponseEntity.ok(new AuthResponseDto(newAccessToken, newRefreshToken));
+        } catch (JwtException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
 }
 
