1f6d429ed1cf8f731110f5af44dadcac4883ddb1
Ashutosh <Ashutosh>
2025-06-28 13:41:31 +0530

    Accepting multiple images in API input

--- service-autocaption/src/main/java/com/image/autocaption/resource/AutoCaptionImageResource.java
+++ service-autocaption/src/main/java/com/image/autocaption/resource/AutoCaptionImageResource.java
@@ -30,22 +30,22 @@ public class AutoCaptionImageResource {
      * The end point that generates a list of 3 captions for the input image. In case, an image is already available
      * in the in-memory cache, the pre-computed list of captions is result from the cache.
      *
-     * @param imageFile the image for which captions are to be generated
+     * @param imageFiles the image for which captions are to be generated
      * @return a response entity holding the list of captions generated and the HTTP status code
      * @throws IOException
      */
     @PostMapping(value = "/captions", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<String>> createCaptions(@RequestParam("image") MultipartFile imageFile) throws IOException {
-        if (imageFile.isEmpty()) {
+    public ResponseEntity<List<String>> createCaptions(@RequestParam("image") MultipartFile[] imageFiles) throws IOException {
+        if (imageFiles[0].isEmpty()) {
             throw new EmptyFileException("Uploaded file is empty");
         }
 
-        String contentType = imageFile.getContentType();
+        String contentType = imageFiles[0].getContentType();
         if (contentType.isEmpty() || !getSupportedContentTypes().containsKey(contentType)) {
             throw new UnsupportedMediaTypeException("Unsupported file type: " + contentType);
         }
 
-        var captions = autoCaptionImageService.createCaptions(imageFile.getBytes(), imageFile);
+        var captions = autoCaptionImageService.createCaptions(imageFiles[0].getBytes(), imageFiles[0]);
 
         return ResponseEntity.ok(captions);
     }
